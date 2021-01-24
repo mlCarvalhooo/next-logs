95144ade4a01820d112cef566adcd467e5a9dff4
Carvalhooo <ybyonicgamer@gmail.com>
2021-01-24 19:42:15 +0000

    Fixed log.js

--- pages/api/log.js
+++ pages/api/log.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import nextConnect from 'next-connect';
-import middleware from '../../database/database';
+//import middleware from '../../database/database';
 const Pusher = require('pusher');
 
 const handler = nextConnect();
@@ -9,16 +9,16 @@ handler.use(middleware);
 
 handler.get(async (req, res) => {
 
-	const doc = await req.db.collection('Devices').findOne()
+	//const doc = await req.db.collection('Devices').findOne()
 	res.json({
 		status: 'SUCCESS',
-		data: doc
+		data: null//doc
 	});
 });
 
 handler.post(async (req, res) => {
 	if (req.body.message) {
-		const doc = await req.db.collection('Devices').findOneAndUpdate({}, { $push: { logs: { message: req.body.message, time: new Date().getTime() } } })
+		//const doc = await req.db.collection('Devices').findOneAndUpdate({}, { $push: { logs: { message: req.body.message, time: new Date().getTime() } } })
 		res.json({
 			status: 'SUCCESS'
 		});
